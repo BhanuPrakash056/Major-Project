9bdef5e5250a1144e08599d0e39e7b9def72be65
bhanuprakash056 <r19cs056@cit.reva.edu.in>
2022-10-20 21:25:06 +0530

    added comments

--- controller/auth.js
+++ controller/auth.js
@@ -5,13 +5,15 @@ import { errorResponse } from '../interceptor/error.js';
 const addUser = async (req,res) => {
     let {name,number,password} = req.body;
     try{ 
+        //checking if the user is already exists
         await UserDetails.find({number:number},(err,data)=>{
         var keycount  = Object.keys(data).length;
-        
+        //if user already exists
         if(keycount > 0){
             return errorResponse(res,404,"User already exists")
         }
         else{
+        //adding a new user 
         const user = new UserDetails()
         user.username = name;
         user.number= number;
